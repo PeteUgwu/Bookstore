6ccb9587971571f1e7b38b18a243d40e86bc6c27
peter <peterugwu64@gmail.com>
2022-12-21 23:26:32 +0100

    add react components

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <>
-    <div>
-      <button type="button">Check Status</button>
-    </div>
-  </>
-);
+const Categories = () => {
+  const categories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div>
+        <div>
+          <span>{categories}</span>
+        </div>
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(checkStatus());
+          }}
+        >
+          Check Status
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Categories;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,14 +3,30 @@ const ADD_BOOK = 'books/books/ADD_BOOK';
 const REMOVE_BOOK = 'books/books/REMOVE_BOOK';
 
 // reducer
-const initialState = [];
+const initialState = [
+  {
+    title: 'Christmas',
+    author: 'Peter',
+    id: 1,
+  },
+  {
+    title: 'New Year',
+    author: 'Valentine',
+    id: 2,
+  },
+];
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case ADD_BOOK:
-      return state;
+      return [
+        ...state,
+        action.payload,
+      ];
     case REMOVE_BOOK:
-      return state;
+      return [
+        ...state.filter((item) => item.id !== action.id),
+      ];
     default:
       return state;
   }
